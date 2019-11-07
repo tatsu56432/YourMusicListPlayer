754ea202e00d88f19ffa65b667e88df65b016443
t_nakajima <t_nakajima@bbmedia.co.jp>
2019-11-07 22:11:20 +0900

    良い感じ！！

--- htdocs/assets/js/script.js
+++ htdocs/assets/js/script.js
@@ -7,13 +7,12 @@
 
     app.Vue.el = "#js-vue";
     app.Vue.methods = {},
-        app.Vue.watch = {},
-        app.Vue.computed,
-        app.Vue.created,
-        app.Vue.mounted,
-
-        app.Vue.data = {},
-        app.Vue.data.items = [];
+    app.Vue.watch = {},
+    app.Vue.computed,
+    app.Vue.created,
+    app.Vue.mounted,
+    app.Vue.data = {},
+    app.Vue.data.items = [];
     app.Vue.data.youtubeData = [];
     app.Vue.data.youtubeDataApiParam = {
         part: 'snippet',
@@ -22,9 +21,17 @@
         key: 'AIzaSyBmMNSFmsKQTMUMlN1qxXHlQjBlOPINuDs'
     }
 
-    // app.music.methods = {},
-    // app.music._songs = [],
-    // app.music.player = {};
+
+    app.Music.methods = {},
+    app.Music._songs = [],
+    app.Music.player = {},
+    app.Music.autoplay = !0,
+    app.Music.sortby = "songID",
+    app.Music.filterby = "all",
+    app.Music.nowplaying = {
+            songID: null,
+            state: "stop"
+        },
 
     app.Vue.methods.getYoutubeData = function () {
         return axios.get('https://www.googleapis.com/youtube/v3/playlistItems', {params: this.youtubeDataApiParam})
@@ -36,6 +43,8 @@
         }
     }
 
+
+
     app.Vue.created = async function () {
 
     }
@@ -45,6 +54,81 @@
         this.getYoutubeData_snippets(response)
         console.log(this.youtubeData)
     }
+
+
+    app.Vue.methods.onClickPlay = function (e) {
+
+        console.log(e.currentTarget.getAttribute("videoId"))
+
+        if (e.currentTarget.getAttribute("videoId") == app.Music.nowplaying.songID){
+
+            switch (app.Music.nowplaying.state) {
+                case "play":
+                    app.Music.methods.pause();
+                    break;
+                case "pause":
+                    // app.Music.methods.reStart()
+            }
+
+        }else{
+
+            app.Music.methods.start(e)
+        }
+
+
+
+
+    }
+
+
+    app.Music.methods.createYoutube = function (videoId) {
+
+        if ("" != videoId) {
+            document.getElementById("js-frame").innerHTML = "",
+            "object" == typeof app.Music.player && "destroy" in app.Music.player && app.Music.player.destroy(), app.Music.player = {};
+
+            var playerParams = {
+                videoId: videoId,
+                playerVars: {
+                    autoplay: 1,
+                    playsinline: 1
+                },
+                events: {
+                    onReady: app.Music.methods.onYoutubeReady,
+                    // onStateChange: e.Music.methods.onYoutubeStateChange,
+                    // onError: e.Music.methods.onYoutubeError
+                }
+            };
+            app.Music.player = new YT.Player("js-frame", playerParams)
+        } else {
+            app.Music.methods.stop()
+        }
+    }
+
+    app.Music.methods.start = function (e) {
+        app.Music.methods.setNowplaying(e.currentTarget.getAttribute("videoId"), "play"),
+        app.Music.methods.createYoutube(e.currentTarget.getAttribute("videoId"))
+
+    }
+
+    app.Music.methods.onYoutubeReady = function(e) {
+        console.log("onYoutubeReady(): " + e.target.getVideoData().video_id),
+        e.target.playVideo()
+    }
+
+    app.Music.methods.setNowplaying = function(videoID, state) {
+
+        app.Music.nowplaying.songID = videoID,
+            app.Music.nowplaying.state = state,
+
+            // app.Vue.data.items.map(function(t) {
+            //     return t.songID == e.music.nowplaying.songID ? t.state = s : t.state = "stop",
+            //         t
+            // }),
+
+            document.getElementById("js-frame").setAttribute("state", state)
+    }
+
 
     new Vue(app.Vue)
 
