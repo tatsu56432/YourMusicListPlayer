20552189fb2e8ebba189a1cea58e27c9f800c2bd
t_nakajima <t_nakajima@bbmedia.co.jp>
2019-11-07 18:39:28 +0900

    player layaout

--- htdocs/assets/js/script.js
+++ htdocs/assets/js/script.js
@@ -6,18 +6,29 @@
         Music: {}
     }
 
+
     app.Vue.el = "#js-vue";
     app.Vue.methods = {},
     app.Vue.watch = {},
+    app.Vue.computed,
+    app.Vue.mounted,
+
     app.Vue.data = {},
     app.Vue.data.items = [];
+    app.Vue.data.youtubeData = [];
+    app.Vue.data.youtubeDataApiParam = {
+        part: 'snippet',
+        playlistId: 'PLlVlyGVtvuVnJ_wdbrb_PUKtndQqzU4me', // 再生リストID
+        maxResults: 30, // デフォルトは5件
+        key: 'AIzaSyBmMNSFmsKQTMUMlN1qxXHlQjBlOPINuDs'
+    }
 
     // app.music.methods = {},
     // app.music._songs = [],
     // app.music.player = {};
 
     app.Vue.methods.getYoutubeData = function () {
-        axios.get('https://www.bbmedia.co.jp/wp-json/wp/v2/works_api')
+        axios.get('https://www.googleapis.com/youtube/v3/playlistItems',{params: this.youtubeDataApiParam})
             .then(function (response) {
                 console.log(response)
             })
@@ -27,6 +38,15 @@
     }
 
 
+    app.Vue.mounted = function () {
+        this.getYoutubeData();
+    }
+
+
+
+
+
+    new Vue(app.Vue)
 
 
 
